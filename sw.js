7916bede3d20585c9a463d9606b5e306e9fae9df
alnaafi3iya-glitch <al.naafi3iya@gmail.com>
2026-09-13 21:56:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'mihrab-cache-';
-const CACHE_NAME = `${CACHE_PREFIX}v114`;
+const CACHE_NAME = `${CACHE_PREFIX}v115`;
 
 // أسماء مؤكدة من ملفي التثبيت وصفحة الإدارة المرفقة.
 // يُحفظ كل ملف بصورة مستقلة حتى لا يمنع ملف مفقود حفظ بقية التطبيق.
